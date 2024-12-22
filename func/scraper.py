1d2f5a8320dece1cb6d2b3504d6ec930e8e796c5
haksolot <sean.lamet.mail@gmail.com>
2024-12-22 22:45:06 +0100

    refactor: reorganize scraper functions and update file structure

--- func/scraper.py
+++ func/scraper.py
@@ -2,7 +2,6 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import json
-import re
 
 def scrape_seller_infos(base_url):
     result = {}
@@ -18,17 +17,16 @@ def scrape_seller_infos(base_url):
     return result
 
 def write_seller_infos(infos, path):
-    with open(path + "/" + "seller_infos.json", "w") as file:
+    os.makedirs(path + "/" + sanitize_filename(infos['name']), exist_ok=True)
+    with open(path + "/" + sanitize_filename(infos['name']) + "/" + "seller_infos.json", "w") as file:
         json.dump(infos, file, indent=4) 
 
-def scrape_album_infos(base_url):
-    result = []
+def scrape_albums(base_url, output_dir):
     catalogue = base_url + "/albums?tab=gallery"
     response = requests.get(catalogue)
     if response.status_code != 200:
         print(f"Error trying to get sellers Yupoo page: ({response.status_code})")
         return
-    
     soup = BeautifulSoup(response.text, 'html.parser')
     albums = soup.find_all('div', class_='showindex__children')
 
@@ -41,42 +39,57 @@ def scrape_album_infos(base_url):
             "link": album_link,
             "image_number": album_image_number
         }
-        result.append(info_obj)
-
+        write_album_infos(info_obj, output_dir)
         response = requests.get(album_link)
         if response.status_code != 200:
             print(f"Error trying to get sellers Yupoo page: ({response.status_code})")
             return
         soup = BeautifulSoup(response.text, 'html.parser')
-        articles = soup.find_all('div', class_='showalbum__children image__main')
-        date = soup.find('time', class_='text_overflow').text.strip()
-        info_obj['date'] = date
-        images = []
-        for article in articles:
-            image_infos = {}
-            image_infos['file_name'] = article.find('h3').text.strip()
-            image_infos['file_link'] = "http:" + article.find('img')['data-origin-src']
-            images.append(image_infos)
-        info_obj['images'] = images
-    return result
+        medias = soup.find_all('div', class_='showalbum__children image__main')
+        for media in medias:
+            if(media.find('div')['data-type'] == 'photo'):
+                new_output_dir = output_dir + "/" +sanitize_filename(info_obj['name']) + "/photos"
+                write_album_photo(media, new_output_dir, base_url)
+            elif(media.find('div')['data-type'] == 'video'):
+                new_output_dir = output_dir + "/" +sanitize_filename(info_obj['name']) + "/videos"
+                write_album_video(media, new_output_dir, base_url)
+    return 
 
-def write_album_infos(infos, path):
-    with open(path + "/" + "album_infos.json", "w") as file:
+def write_album_infos(infos, output_dir):
+    os.makedirs(output_dir + "/" + sanitize_filename(infos['name']), exist_ok=True)
+    os.makedirs(output_dir + "/" + sanitize_filename(infos['name']) + "/photos" , exist_ok=True)
+    os.makedirs(output_dir + "/" + sanitize_filename(infos['name']) + "/videos" , exist_ok=True)
+    with open(output_dir + "/" + sanitize_filename(infos['name']) + "/" +"album_infos.json", "w") as file:
         json.dump(infos, file, indent=4) 
-        
-def media_download(url, name, path):
-    _, ext = os.path.splitext(url)
-    if not ext:
-        print("Impossible de déterminer le suffixe à partir de l'URL.")
-        return
-    response = requests.get(url, stream=True)
+
+def write_album_photo(media, output_dir, base_url):
+    image_name = media.find('h3').text.strip()
+    image_link = "http:" + media.find('img')['data-origin-src']
+    headers = {
+        "Referer": base_url
+    }
+    response = requests.get(image_link, stream=True, headers=headers)
     if response.status_code != 200:
-            print(f"Error trying to get sellers Yupoo page: ({response.status_code})")
-            return
-    full_name = name + ext
-    with open(path + "/" + full_name, "wb") as file:
+        print(f"Error trying to scrape image: ({response.status_code})")
+        return
+    with open(output_dir + "/" + image_name, "wb") as file:
         for chunk in response.iter_content(1024):
             file.write(chunk)
+
+def write_album_video(media, output_dir, base_url):
+    video_name = media.find('h3').text.strip()
+    video_link = "http://uvd.yupoo.com" + media.find('img')['data-path']
+    headers = {
+        "Referer": base_url
+    }
+    response = requests.get(video_link, stream=True, headers=headers)
+    if response.status_code != 200:
+        print(f"Error trying to scrape image: ({response.status_code})")
+        return
+    with open(output_dir + "/" + video_name, "wb") as file:
+        for chunk in response.iter_content(1024):
+            file.write(chunk)
+
 
 def sanitize_filename(filename):
     replacements = {
@@ -95,19 +108,3 @@ def sanitize_filename(filename):
         filename = filename.replace(char, replacement)
     
     return filename
-
-def build_seller_page(base_url, path):
-    seller_infos = scrape_seller_infos(base_url)
-    os.makedirs(path + "/" + sanitize_filename(seller_infos['name']), exist_ok=True)
-    with open(path + "/" + sanitize_filename(seller_infos['name']) + "/data.json", 'w') as file:
-        json.dump(seller_infos, file, indent=4)
-    albums_infos = scrape_album_infos(base_url)
-    for album in albums_infos:
-        album_path = path + "/" + sanitize_filename(seller_infos['name']) + "/" + sanitize_filename(album['name'])
-        os.makedirs(album_path, exist_ok=True)
-        with open(album_path + "/data.json", 'w') as file:
-            json.dump(album, file, indent=4)
-        for image in album['images']:
-            media_download(image['file_link'], sanitize_filename(image['file_name']), album_path)
-
-build_seller_page("https://no1factory.x.yupoo.com", ".")
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-from scraping import build_seller_page
+from func import *
+import argparse
 
-build_seller_page("", "")
+parser = argparse.ArgumentParser(description="Download content of a Yupoo album page")
+
+parser.add_argument(
+    "--link", type=str,
+    required=True,
+    help="Link of the Yupoo album"
+)
+
+parser.add_argument(
+    "--output_dir", type=str,
+    required=True,
+    help="Output directory path"
+)
+
+args = parser.parse_args()
+
+def main():
+    seller_infos = scrape_seller_infos(args.link)
+    write_seller_infos(seller_infos, args.output_dir)
+    scrape_albums(args.link, args.output_dir + "/" + sanitize_filename(seller_infos['name']))
+    
+if __name__ == "__main__":
+    main()
